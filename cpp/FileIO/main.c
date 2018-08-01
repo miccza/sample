a29e0fd01fc73f8d683461248baea368ee4da725
Jedrzej Mrugala <jedrzej.mrugala@nokia.com>
2018-08-01 16:30:26 +0200

    Removed solutions from C/C++ tasks

--- cpp/FileIO/main.c
+++ cpp/FileIO/main.c
@@ -7,31 +7,6 @@
 
 int main()
 {
-    const char* file = "input.txt";
-    const char* mode = "r";
-    FILE* fd = fopen(file, mode);
-    unsigned buff;
-
-    if(fd)
-    {
-        printf("File %s opened correctly.\n", file);
-        int sum = 0;
-
-        while( fscanf(fd, "%d", &buff) != EOF )
-        {
-            printf("Read: %d.\n", buff);
-            sum += buff;
-        }
-
-        printf("The sum is: %d\n", sum);
-
-    }
-    else
-    {
-        printf("There was an error during opening the file %s.\n", file);
-    }
-
-    printf("Closing file.\n");
-    fclose(fd);
+    // your code here
     return 0;
 }
--- cpp/Number/Number.cpp
+++ cpp/Number/Number.cpp
@@ -1,39 +1,33 @@
 #include "Number.hpp"
-#include <cmath>
 
 void Number::setNumber(const int new_val)
 {
     // your code here
     //
     //
-    this->value = new_val;
 }
 
 int Number::getNumber() const
 {
     // your code here
-    // return 0;
-    return this->value;
+    return 0;
 }
 
 std::string Number::print() const
 {
     // your code here
-    // return "";
-    return std::string("I'm a Number class with value " + std::to_string(this->value) + ".");
+    return "";
 }
 
 bool Number::isNegative() const
 {
     // your code here
-    // return false;
-    return this->value < 0;
+    return false;
 }
 
 long Number::getAbsolute() const
 {
     // your code here
     // Try to use STL here
-    // return 0;
-    return std::abs(this->value);
+    return 0;
 }
